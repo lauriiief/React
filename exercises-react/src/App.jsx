d21a9f2bfc44b6027dcac71d4b846c66c314528c
laura <lauraespanyol95@gmail.com>
2023-12-13 09:25:22 +0100

    Components 2

--- exercises-react/src/App.jsx
+++ exercises-react/src/App.jsx
@@ -1,9 +1,11 @@
 import { Hello } from "./Hello";
+import { Message } from "./Paragraf";
 
 export function App() {
     return (
         <div>
             <Hello />
+            <Message />
         </div>
     )
 }
