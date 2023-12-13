645143c2a2331d9c3ba0a5ff38927e927ae4073e
laura <lauraespanyol95@gmail.com>
2023-12-13 09:40:11 +0100

    Components 3

--- exercises-react/src/App.jsx
+++ exercises-react/src/App.jsx
@@ -1,11 +1,20 @@
 import { Hello } from "./Hello";
 import { Message } from "./Paragraf";
 
-export function App() {
+{/*export function App() {
     return (
         <div>
             <Hello />
+            <Hello />  it renders anyway but two hello worlds 
             <Message />
         </div>
     )
-}
+} */}
+
+export function App(){
+    return (
+    <div>
+        <Message /> 
+    </div>
+    )
+}{/* it renders only the Message */}
