dadcc140585ea7f9c67a74b7cc491644a610ce9f
laura <lauraespanyol95@gmail.com>
2023-12-18 10:52:01 +0100

    Handling Events - 2

--- exercises-react/src/MouseClicker.jsx
+++ exercises-react/src/MouseClicker.jsx
@@ -1,11 +1,17 @@
 export function MouseClicker() {
     function handleButtonClick(event){
-        console.log(event.currentTarget.name)
+        let button = event.currentTarget;
+        let image = button.firstChild;
+        let src = image.getAttribute("src");
+        console.log(src);
     }
 
     return (
         <div>
-            <button name="one" onClick={handleButtonClick}> Click me </button>
+            <button name="one" onClick={handleButtonClick}> 
+            <img src="#"/>
+            Click me 
+            </button>
         </div>
     )
 }
