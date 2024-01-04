300a462de3e5c72ba4ec9ed75f3f3fad937279bc
laura <lauraespanyol95@gmail.com>
2024-01-04 16:31:34 +0100

    Controlled Forms - 5

--- exercises-react/src/Login.jsx
+++ exercises-react/src/Login.jsx
@@ -29,8 +29,32 @@ export function Login({loginFunction}) { // callback = una funcion que se pasa c
         setData(createData())
     }
 
+    function handleSubmit(event){
+        event.preventDefault();
+        if(data.username && data.password){
+            loginFunction(data);
+        }
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        if(data.username && data.password){
+            loginFunction(data);
+        }
+    }
+
     return (
         <div>
+            <form id="myForm">
+                <p>Inside form element</p>
+                <input name="username" type="text" value={data.username} onChange={handleInputChange}/>
+                <input name='password' type='password' value={data.password} onChange={handleInputChange} />
+                <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange}/>remember
+                <button onClick={handleReset}>Reset</button>
+                <input type="submit" value="Login"/>
+            </form>
+
+            <p>Outside form element</p>
             <input name='username' value={data.username} onChange={handleInputChange} />
             <input name='password' type='password' value={data.password} onChange={handleInputChange} />
             <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange}/>remember
