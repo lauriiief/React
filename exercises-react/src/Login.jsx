bc5054e73566a8e5c005ee818d5c638cdddf73a7
laura <lauraespanyol95@gmail.com>
2024-01-04 15:54:15 +0100

    Controlled Forms - 4

--- exercises-react/src/Login.jsx
+++ exercises-react/src/Login.jsx
@@ -25,12 +25,17 @@ export function Login({loginFunction}) { // callback = una funcion que se pasa c
         })
     }
 
+    function handleReset(){
+        setData(createData())
+    }
+
     return (
         <div>
             <input name='username' value={data.username} onChange={handleInputChange} />
             <input name='password' type='password' value={data.password} onChange={handleInputChange} />
             <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange}/>remember
             <button disabled={!data.username || !data.password} onClick={() => loginFunction(data)}>Login</button>  {/* pasamos la funcion con una arrow function porque sino se estaria ejecutando todo el rato */}      
+            <button onClick={handleReset}>Reset</button>
         </div>
     )
 }
