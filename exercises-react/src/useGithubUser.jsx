672c0fd1af93bc950dba121d32a9430602db5fa2
Laura <lauraespanyol95@gmail.com>
2024-01-30 09:51:03 +0100

    SWR - 2

--- exercises-react/src/useGithubUser.jsx
+++ exercises-react/src/useGithubUser.jsx
@@ -3,6 +3,14 @@ import useSWR from 'swr'
 const fetcher = url => fetch(url).then(response => response.json())
 
 export function useGithubUser(username) {
+    if(!username){
+        return{
+            users: null,
+            error: null,
+            isLoading: false,
+            onfetchUser: () => {},
+        }
+    }
     
     const { data, error, mutate } = useSWR(`https://api.github.com/users/${username}`, fetcher)
 
